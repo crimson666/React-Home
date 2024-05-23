573cf5537242895b9dc83500d9291fcb972d52a7
crimson666 <ankurbsws@gmail.com>
2024-05-24 01:47:03 +0530

    Stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Messege from "./Messege";
 import ListGroup from "./components/ListGroup";
 
-
-function App(){
-  return <div><ListGroup/></div>;
-
+function App() {
+  let items = ["apple", "MI", "Nokia", "LG"];
+  const itemSelected = (itemSelected: string) => {
+    console.log("selected item " + itemSelected);
+  };
+  return (
+    <div>
+      <ListGroup
+        listBla={items}
+        heading="Mobile Brands"
+        onSelect={itemSelected}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import { Fragment } from "react/jsx-runtime";
+// import { MouseEvent } from "react";
 
-function ListGroup() {
-  let listBla = ["apple", "MI", "Nokia", "LG"];
+interface listProps {
+  listBla: string[];
+  heading: string;
+  onSelect: (itemSelected: string) => void;
+}
+
+function ListGroup({ listBla, heading, onSelect }: listProps) {
+  const [selectedItem, setSelectItemIndex] = useState(-1);
   //JS Magic if some condition && something, if 1st one is true it will return the 2nd condition
   const ifNoList = () => {
     return listBla.length === 0 && <h1> No Iteams</h1>;
   };
+
+  // const handleClickEvent = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
+
   return (
     <Fragment>
-      <h1>Some random List</h1>
+      <h1>Some random List of {heading}</h1>
       {ifNoList()}
       <ul className="list-group">
-        {listBla.map((item) => (
-          <li key={item}>{item}</li>
+        {listBla.map((item, index) => (
+          <li
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectItemIndex(index);
+              onSelect(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
