ec34dc20bddc8d4bfb8b6fa85651a81e7b9c0581
crimson666 <ankurbsws@gmail.com>
2024-05-26 15:11:16 +0530

    router stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Messege from "./Messege";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   let items = ["apple", "MI", "Nokia", "LG"];
@@ -14,26 +15,33 @@ function App() {
   };
 
   return (
-    <div>
-      <ListGroup
-        listBla={items}
-        heading="Mobile Brands"
-        onSelect={itemSelected}
-      />
-      {/* <Alert Data={"Stuff to show"} /> */}
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          Some Stuff <h1>this is Bold </h1>goes here
-        </Alert>
-      )}
-      <Button
-        color={"success"}
-        data={"Some Random button"}
-        onClick={() => {
-          setAlertVisibility(true);
-        }}
-      />
-    </div>
+    <Router>
+      <div>
+        <ListGroup
+          listBla={items}
+          heading="Mobile Brands"
+          onSelect={itemSelected}
+        />
+        {/* <Alert Data={"Stuff to show"} /> */}
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>
+            Some Stuff <h1>this is Bold </h1>goes here
+          </Alert>
+        )}
+        {/* <Switch>
+          <Route exact path="/SomePath">
+        <SomeComponent/>  
+        <Route/>
+        <Switch/> */}
+        <Button
+          color={"success"}
+          data={"Some Random button"}
+          onClick={() => {
+            setAlertVisibility(true);
+          }}
+        />
+      </div>
+    </Router>
   );
 }
 
